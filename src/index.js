05c8a527c2c4f0a4bf2248c5ce8a58952ab033d5
Oli Zimpasser <oglimmer@gmail.com>
2017-02-18 00:51:00 +0100

    Fix production version of src/index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './components/Root';
 
+import configureStore from './util/configureStore';
+
+let state;
+if (window.$REDUX_STATE) {
+  state = window.$REDUX_STATE;
+}
+
+const store = configureStore(state);
+
 ReactDOM.render(
-  <Root />,
+  <Root store={store} />,
   document.getElementById('root'),
 );
+
