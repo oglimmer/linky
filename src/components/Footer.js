f0ad1b02c4774fa78a6c0c9d9daab7c15600acc8
Oli Zimpasser <oglimmer@gmail.com>
2017-03-25 14:44:36 +0100

    Fix link to github

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,8 +6,8 @@ import BuildInfo from '../util/BuildInfo';
 export default () => (
   <div>
     Copyright 2017 by oglimmer.de - Build
-    on {BuildInfo.BUILDDATE} from {BuildInfo.BRANCHNAME} at
-    <a href="https://github.com/oglimmer/linky/commit/{BuildInfo.COMMITHASH}">
+    on {BuildInfo.BUILDDATE} from {BuildInfo.BRANCHNAME}{' '}
+    at <a href={`https://github.com/oglimmer/linky/commit/${BuildInfo.COMMITHASH}`}>
       {BuildInfo.COMMITHASH}
     </a>
   </div>
