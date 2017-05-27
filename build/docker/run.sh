8c2dd68060d00f52bd83e98e324cc61ff4e5de96
Oli Zimpasser <oglimmer@gmail.com>
2017-05-27 23:28:38 +0200

    Fix docker/run.sh

--- build/docker/run.sh
+++ build/docker/run.sh
@@ -4,6 +4,9 @@ if [ "$1" = "" ] || [ "$1" = "help" ] || [ "$1" = "--help" ] || [ "$1" = "-h" ];
   echo 'Use `git` or `local` or `clean`.'
   exit 1
 elif [ "$1" = "git" ]; then
+  #
+  # use head of master from git and run tests against a fresh build
+  #
   [ -f from_local ] && echo "local build in place. use clean before doing a git build." && exit 2
   touch from_git
   if ! [ -d sources ]; then
@@ -12,6 +15,9 @@ elif [ "$1" = "git" ]; then
     cd sources && git pull && cd ..
   fi
 elif [ "$1" = "local" ]; then
+  #
+  # use a copy of this project and run tests against a fresh build
+  #
   [ -f from_git ] && echo "git build in place. use clean before doing a local build." && exit 2
   touch from_local
   mkdir -p ./sources
@@ -20,23 +26,23 @@ elif [ "$1" = "local" ]; then
   do
     if [ "$file" != "." ] && [ "$file" != ".." ] \
       && [ "$file" != "node_modules" ] && [ "$file" != "build" ]; then
-      cp -r ../../$file ./sources/$file
+      cp -rf ../../$file ./sources/
     fi
   done
   mkdir -p ./sources/build
-  cp ../webpack.dev.config.js ./sources/build/webpack.dev.config.js
-  cp ../webpack.prod.config.js ./sources/build/webpack.prod.config.js
-  cp -r ../couchdb ./sources/build/
+  cp -f ../webpack.dev.config.js ./sources/build/webpack.dev.config.js
+  cp -f ../webpack.prod.config.js ./sources/build/webpack.prod.config.js
+  cp -rf ../couchdb ./sources/build/
 elif [ "$1" = "clean" ]; then
-  rm -rf sources
-  rm -f from_git
-  rm -f from_local
+  rm -f from_git && echo "git marker deleted"
+  rm -f from_local && echo "local marker deleted"
+  rm -rf sources && echo "sources deleted"
   exit 0
 else
   echo 'Illegal param `'$1'`. Use `git` or `local` or `help`.'
   exit 1
 fi
 
-docker-compose build
+docker-compose up --build --abort-on-container-exit
 
-docker-compose up
+docker-compose down
