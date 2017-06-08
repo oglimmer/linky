e16e1e910316997abcf0bdd8dbe74a8330bd6e28
Oli Zimpasser <oglimmer@gmail.com>
2017-06-08 22:04:46 +0200

    Log db config on start

--- server/dao/NanoConnection.js
+++ server/dao/NanoConnection.js
@@ -1,16 +1,21 @@
 
 import nano from 'nano';
+import winston from 'winston';
 
 import properties from '../util/linkyproperties';
 
 const dbProps = properties.server.db;
 const url = `${dbProps.protocol}://${dbProps.host}:${dbProps.port}/${dbProps.name}`;
 
-const linkyDb = nano({
+const config = {
   url,
   requestDefaults: {
     rejectUnauthorized: dbProps.rejectUnauthorized,
   },
-});
+};
+
+winston.loggers.get('application').debug('DB config is %j', config);
+
+const linkyDb = nano(config);
 
 export default linkyDb;
