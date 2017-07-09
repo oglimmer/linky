9398ec0c47794381eb4c808c9adc7c7e33393699
Oli Zimpasser <oglimmer@gmail.com>
2017-07-09 17:23:48 +0200

    Improse test.sh

--- build/test/test.sh
+++ build/test/test.sh
@@ -6,43 +6,47 @@ fi
 REST_URL=$BASE_URL/rest
 
 if [ "$1" == "createuser" ]; then
-
-	curl -s -X POST --data '{"email":"foo@test.com","password":"foo"}' -H "Content-Type: application/json" $REST_URL/users
-
+  curl -s -X POST --data '{"email":"foo@test.com","password":"foo"}' \
+    -H "Content-Type: application/json" $REST_URL/users
 fi
 
 if [ "$1" == "authenticate" ]; then
-  authResp=$(curl -s -X POST --data '{"email":"foo@test.com","password":"foo"}' -H "Content-Type: application/json" $REST_URL/authenticate)
+  authResp=$(curl -s -X POST \
+    --data '{"email":"foo@test.com","password":"foo"}' \
+    -H "Content-Type: application/json" $REST_URL/authenticate)
   if [ "$authResp" != "" ]; then
     token=$(echo "$authResp" | json token)
     echo "export AUTH_TOKEN=$token"
   else
-    echo "Call to /authenticate was empty"
+    echo "Call to /authenticate resulted in empty"
   fi
 fi
 
 if [ "$1" == "createlink" ]; then
-
-	curl -s -X POST --data '{"url":"http://oglimmer.de","tags":"","notes":"","pageTitle":"","rssUrl":""}' -H "Content-Type: application/json" -H "authorization: Bearer $AUTH_TOKEN" $REST_URL/links
-
+  [ -z "$AUTH_TOKEN" ] && echo "AUTH_TOKEN not set" && exit 1
+  curl -s -X POST \
+    --data '{"url":"http://oglimmer.de","tags":"","notes":"","pageTitle":"","rssUrl":""}' \
+    -H "Content-Type: application/json" -H "authorization: Bearer $AUTH_TOKEN" $REST_URL/links
 fi
 
 if [ "$1" == "getlinks" ]; then
-
-	curl -s -X GET -H "authorization: Bearer $AUTH_TOKEN" $REST_URL/links/all
-
+  [ -z "$AUTH_TOKEN" ] && echo "AUTH_TOKEN not set" && exit 1
+  curl -s -X GET -H "authorization: Bearer $AUTH_TOKEN" $REST_URL/links/all
 fi
 
 if [ "$1" == "deletelink" ]; then
-
-	curl -s -X DELETE -H "authorization: Bearer $AUTH_TOKEN" $REST_URL/links/$2
-
+  [ -z "$AUTH_TOKEN" ] && echo "AUTH_TOKEN not set" && exit 1
+  curl -s -X DELETE -H "authorization: Bearer $AUTH_TOKEN" $REST_URL/links/$2
 fi
 
-if [ "$1" == "portalpage" ]; then
-  curl -b "authToken=$AUTH_TOKEN" "$BASE_URL/portalPage"
+if [ "$1" == "html" ]; then
+  [ -z "$AUTH_TOKEN" ] && echo "AUTH_TOKEN not set" && exit 1
+  url=$2
+  [ -z "$url" ] && url=links/portal
+  curl --cookie "authToken=$AUTH_TOKEN" "$BASE_URL/$url"
 fi
 
 if [ "$1" == "hierachy" ]; then
+  [ -z "$AUTH_TOKEN" ] && echo "AUTH_TOKEN not set" && exit 1
   curl -s -X GET -H "authorization: Bearer $AUTH_TOKEN" $REST_URL/tags/hierachy
 fi
