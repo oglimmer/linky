9d26038275c8106515c300e0cd78e0198ffa3512
Oli Zimpasser <oglimmer@gmail.com>
2017-07-29 16:28:48 +0200

    Fix broken eslint validation

--- server/util/postStartupClean.js
+++ server/util/postStartupClean.js
@@ -4,6 +4,6 @@ import asyncWaitDao from '../dao/asyncWaitDao';
 export default () => {
   /* eslint-disable no-underscore-dangle */
   asyncWaitDao.getAllAsyncWaits()
-    .then(rows => rows.forEach((rec) => asyncWaitDao.delete(rec._id, rec._rev)));
+    .then(rows => rows.forEach(rec => asyncWaitDao.delete(rec._id, rec._rev)));
 /* eslint-enable no-underscore-dangle */
 };
