a77d8209d882b95e1f1eae85b6d7805ef3e41fee
Oli Zimpasser <oglimmer@gmail.com>
2017-07-29 23:00:31 +0200

    some pages return a favicon url which in turn just returns the page.

--- server/util/favicon.js
+++ server/util/favicon.js
@@ -2,6 +2,7 @@
 import favicon from 'favicon';
 import { Promise } from 'bluebird';
 import request from 'request-promise';
+import winston from 'winston';
 
 const promisedFavicon = Promise.promisify(favicon);
 
@@ -9,7 +10,13 @@ export default url => promisedFavicon(url).then((faviconUrl) => {
   if (!faviconUrl) {
     return null;
   }
-  return request.get(faviconUrl).then((faviconBlob) => {
+  return request.get(faviconUrl, { resolveWithFullResponse: true }).then((response) => {
+    const contentType = response.headers['content-type'];
+    winston.loggers.get('application').debug('Favicon %s has content-type of %s', faviconUrl, contentType);
+    if (contentType && contentType.startsWith('text/html')) {
+      return null;
+    }
+    const faviconBlob = response.body;
     if (faviconBlob) {
       return faviconUrl;
     }
