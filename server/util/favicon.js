930e6a82d633e422909a5b8c6b90d023cda5bec5
Oli Zimpasser <oglimmer@gmail.com>
2017-07-30 13:35:12 +0200

    Only accept favicons for content-types starting with image/

--- server/util/favicon.js
+++ server/util/favicon.js
@@ -2,7 +2,6 @@
 import favicon from 'favicon';
 import { Promise } from 'bluebird';
 import request from 'request-promise';
-import winston from 'winston';
 
 const promisedFavicon = Promise.promisify(favicon);
 
@@ -12,8 +11,7 @@ export default url => promisedFavicon(url).then((faviconUrl) => {
   }
   return request.get(faviconUrl, { resolveWithFullResponse: true }).then((response) => {
     const contentType = response.headers['content-type'];
-    winston.loggers.get('application').debug('Favicon %s has content-type of %s', faviconUrl, contentType);
-    if (contentType && contentType.startsWith('text/html')) {
+    if (!contentType || !contentType.startsWith('image/')) {
       return null;
     }
     const faviconBlob = response.body;
