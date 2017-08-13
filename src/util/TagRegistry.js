45bba07b8698eed561106905df658519c3496c19
Oli Zimpasser <oglimmer@gmail.com>
2017-08-13 16:27:03 +0200

    Comments on TagRegistry

--- src/util/TagRegistry.js
+++ src/util/TagRegistry.js
@@ -1,24 +1,34 @@
 
+// completely forbidden, always removed
+export const ROOT = 'root';
 
+// system set, must never be removed
 export const ALL = 'all';
-export const BROKEN = 'broken';
+export const ARCHIVE = 'archive';
+
+// system set/unset. must never be removed or set by the user
 export const RSS = 'rss';
+export const DUEDATE = 'duedate';
+
+// system set, may be removed at any time
 export const UNTAGGED = 'untagged';
 export const URLUPDATED = 'urlupdated';
+export const DUPLICATE = 'duplicate';
+export const BROKEN = 'broken';
+export const DUE = 'due';
+
+// user set, may be removed at any time
 export const PORTAL = 'portal';
 export const LOCKED = 'locked';
-export const ROOT = 'root';
-export const DUEDATE = 'duedate';
-export const DUE = 'due';
-export const DUPLICATE = 'duplicate';
-export const ARCHIVE = 'archive';
 
 export const TAGS = [ALL, BROKEN, RSS, UNTAGGED, URLUPDATED, PORTAL, LOCKED, ROOT, DUEDATE, DUE,
   DUPLICATE, ARCHIVE];
 
+// displayed in different color and cannot be removed
 export const READONLY_TAGS = [ALL, RSS, ROOT, PORTAL, DUEDATE, DUE, ARCHIVE];
 
-export const FORBIDDEN_TAGS = [ROOT]; // these tokens will be deleted if found
+// not allowed on a link, thus will be deleted if found
+export const FORBIDDEN_TAGS = [ROOT];
 
 export default {
   TAGS,
