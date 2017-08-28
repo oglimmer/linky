b205f9824bd2fa710ce82aa47a304d14c31e5264
Oli Zimpasser <oglimmer@gmail.com>
2017-08-29 01:12:14 +0200

    Use different sha256 lib

--- server/util/HashUtil.js
+++ server/util/HashUtil.js
@@ -1,16 +1,10 @@
 
-import crypto from 'crypto';
+import sha256 from 'crypto-js/sha256';
+import Base64 from 'crypto-js/enc-base64';
+import hex from 'crypto-js/enc-hex';
 
-export const hashSha256Hex = (string) => {
-  const hash = crypto.createHash('sha256');
-  hash.update(string);
-  return hash.digest('hex');
-};
+export const hashSha256Hex = string => hex.stringify(sha256(string));
 
-export const hashSha256Base64 = (string) => {
-  const hash = crypto.createHash('sha256');
-  hash.update(string);
-  return hash.digest('base64');
-};
+export const hashSha256Base64 = string => Base64.stringify(sha256(string));
 
 export default {};
