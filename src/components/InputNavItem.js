2c843d3818595ae178e4df46a37bb534412a7564
Oli Zimpasser <oglimmer@gmail.com>
2017-08-30 02:13:31 +0200

    Show search only on /links

--- src/components/InputNavItem.js
+++ src/components/InputNavItem.js
@@ -2,50 +2,53 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router';
 import { Control, Form, actions } from 'react-redux-form';
 
 import { sendSearch, fetchLinks } from '../redux/actions/links';
 
-const InputNavItem = ({ onSearch, onClear, searchTerm }) => (
+const InputNavItem = ({ onSearch, onClear, searchTerm, location }) => (
   <li role="presentation">
-    <Form
-      className="navbar-form"
-      role="search"
-      model="searchBar"
-      onSubmit={(formData) => { onSearch(formData.searchTerm); }}
-    >
-      <div className="input-group">
-        <Control
-          type="text"
-          className="form-control"
-          placeholder="Search"
-          model=".searchTerm"
-        />
-        <div className="input-group-btn">
-          <button
-            className="btn btn-default"
-            type="button"
-            onClick={() => onSearch(searchTerm)}
-          >
-            <i className="glyphicon glyphicon-search" />
-          </button>
-          { searchTerm ?
+    { location.pathname.startsWith('/links/') ?
+      <Form
+        className="navbar-form"
+        role="search"
+        model="searchBar"
+        onSubmit={(formData) => { onSearch(formData.searchTerm); }}
+      >
+        <div className="input-group">
+          <Control
+            type="text"
+            className="form-control"
+            placeholder="Search"
+            model=".searchTerm"
+          />
+          <div className="input-group-btn">
             <button
               className="btn btn-default"
               type="button"
-              onClick={() => { onClear(); }}
+              onClick={() => onSearch(searchTerm)}
             >
-              <i className="glyphicon glyphicon-ban-circle" />
-            </button> : '' }
+              <i className="glyphicon glyphicon-search" />
+            </button>
+            { searchTerm ?
+              <button
+                className="btn btn-default"
+                type="button"
+                onClick={() => { onClear(); }}
+              >
+                <i className="glyphicon glyphicon-ban-circle" />
+              </button> : '' }
+          </div>
         </div>
-      </div>
-    </Form>
+      </Form> : '' }
   </li>
 );
 InputNavItem.propTypes = {
   onSearch: PropTypes.func.isRequired,
   onClear: PropTypes.func.isRequired,
   searchTerm: PropTypes.string.isRequired,
+  location: PropTypes.shape().isRequired,
 };
 
 const mapStateToProps = state => ({
@@ -68,4 +71,4 @@ const mapDispatchToProps = dispatch => ({
   },
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(InputNavItem);
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(InputNavItem));
