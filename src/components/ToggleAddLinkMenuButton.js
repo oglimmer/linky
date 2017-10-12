87d168eca82138b7d37ff4fcbade6f4980f1e253
Oli Zimpasser <oglimmer@gmail.com>
2017-10-13 00:35:35 +0200

    Always pre-select the currently selected tag for "Add Link"

--- src/components/ToggleAddLinkMenuButton.js
+++ src/components/ToggleAddLinkMenuButton.js
@@ -3,15 +3,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Button } from 'react-bootstrap';
 import { connect } from 'react-redux';
+import { actions } from 'react-redux-form';
 
 import { toggleVisibilityMenuBar } from '../redux/actions';
 
-const ToggleAddLinkMenuButton = ({ onClick, isAddEnabled, authToken }) => {
+const ToggleAddLinkMenuButton = ({ onClick, isAddEnabled, authToken, selectedTag }) => {
   if (!authToken || isAddEnabled) {
     return null;
   }
   return (
-    <Button onClick={onClick}>Add Link</Button>
+    <Button onClick={() => onClick(selectedTag)}>Add Link</Button>
   );
 };
 
@@ -19,15 +20,23 @@ ToggleAddLinkMenuButton.propTypes = {
   onClick: PropTypes.func.isRequired,
   authToken: PropTypes.string.isRequired,
   isAddEnabled: PropTypes.bool.isRequired,
+  selectedTag: PropTypes.string,
+};
+ToggleAddLinkMenuButton.defaultProps = {
+  selectedTag: null,
 };
 
 const mapStateToProps = state => ({
   authToken: state.auth.token,
   isAddEnabled: state.menuBar.addEnabled,
+  selectedTag: state.mainData.selectedTag,
 });
 
 const mapDispatchToProps = dispatch => ({
-  onClick: () => dispatch(toggleVisibilityMenuBar()),
+  onClick: (selectedTag) => {
+    dispatch(toggleVisibilityMenuBar());
+    dispatch(actions.change('addUrl.tags', selectedTag));
+  },
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(ToggleAddLinkMenuButton);
