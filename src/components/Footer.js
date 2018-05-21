43504d4c7e3f568eb3ce8d3e6b0dffc4d1fe06c2
Oli Zimpasser <oglimmer@gmail.com>
2018-05-21 15:50:59 +0200

    Add link for data privacy and update year

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,13 +8,15 @@ import BuildInfo from '../util/BuildInfo';
 
 const Footer = ({ location }) => (
   <div className={location.pathname === '/tags' ? 'footer' : ''}>
-    Copyright 2017 by linky1.com - Build
+    Copyright 2017-2018 by linky1.com - Build
     on {BuildInfo.BUILDDATE} from {BuildInfo.BRANCHNAME}{' '}
     at <a href={`https://github.com/oglimmer/linky/commit/${BuildInfo.COMMITHASH}`}>
       {BuildInfo.COMMITHASH}
     </a>
     {' '} | {' '}
     <Link to="/contact">Contact</Link>
+    {' '} | {' '}
+    <Link to="/help">Data privacy</Link>
   </div>
 );
 Footer.propTypes = {
